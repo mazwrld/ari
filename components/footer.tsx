7d63f142cbd8aad8ad485f90a21c5b610268e984
Maz <mazspm@icloud.com>
2023-06-02 20:05:04 -0500

    fixed typo in footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -10,7 +10,7 @@ export function Footer() {
           <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
             <Image src={Logo} height={34} width={34} alt="logo" />
             <p className="text-center text-sm leading-loose  md:text-left">
-              Built on shadcn . The source code is available on {''}
+              Built on shadcn. The source code is available on{' '}
               <Link
                 href="https://github.com/mazwrld/ari"
                 target="_blank"

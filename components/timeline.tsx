631eafde8018d7f695ce776cc53131493481a774
Maz <mazspm@icloud.com>
2023-06-03 21:28:33 -0500

    fixed hydration error

--- components/timeline.tsx
+++ components/timeline.tsx
@@ -16,7 +16,7 @@ export function Timeline({ timeline }: TimelineProps) {
             <ol className="relative flex flex-col border-l border-stone-200 dark:border-stone-700 md:flex-row">
               <li className="mb-10 ml-4">
                 <div className="absolute -left-1.5 mt-1.5 h-3 w-3 rounded-full border border-white bg-stone-200 dark:border-stone-900 dark:bg-stone-700" />
-                <p className="flex flex-row flex-wrap items-center justify-start gap-4 text-xs md:text-sm">
+                <div className="flex flex-row flex-wrap items-center justify-start gap-4 text-xs md:text-sm">
                   <span className="inline-block rounded-md bg-stone-900 px-2 py-1 font-semibold text-white dark:bg-white dark:text-stone-900">
                     {item.year}
                   </span>
@@ -26,7 +26,7 @@ export function Timeline({ timeline }: TimelineProps) {
                   <div className="my-1 text-sm font-normal leading-none text-stone-400 dark:text-stone-500">
                     {item.duration}
                   </div>
-                </p>
+                </div>
                 <p className="my-2 text-base font-normal text-stone-500 dark:text-stone-400">
                   {item.details}
                 </p>

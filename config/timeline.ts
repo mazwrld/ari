93ca2faa7d941fd2abdb7f88f0d38640ec4464bf
Maz <mazspm@icloud.com>
2023-06-07 20:24:00 -0500

    fixing emojis

--- config/timeline.ts
+++ config/timeline.ts
@@ -13,20 +13,20 @@ export const timelineData = [
     title: 'Software Engineer Internship 🚀',
     duration: '4 months',
     details:
-      'I had the fantastic opportunity to actively contribute to developing an enterprise application for the University of Manitoba as part of an Agile team 🚀. We utilized TypeScript, Angular, Node, and other libraries to create a comprehensive full-stack application. 🛠️🔧 It was a fulfilling experience, and I am grateful for the opportunity to work on such an impactful project for the university.',
+      'I had the fantastic opportunity to actively contribute to developing an enterprise application for the University of Manitoba as part of an Agile team 🚀. We utilized TypeScript, Angular, Node, and other libraries to create a comprehensive full-stack application. 🛠️ It was a fulfilling experience, and I am grateful for the opportunity to work on such an impactful project for the university.',
   },
   {
     year: '2018',
     title: 'Diploma in Business Information Technology 🎓',
     duration: '3 years',
     details:
-      'I was dedicated to obtaining my computer science degree and eagerly absorbing valuable knowledge. 😊📚 Immersed in information technology, I delved into subjects like app development, database management, network administration, and web development. 💻📱🌐 I even glimpsed the business aspects. 💼📈 The most invaluable skill I acquired was the ability to learn. 🎓📚🧠 It will stay with me for a lifetime.',
+      'I was dedicated to obtaining my computer science degree and eagerly absorbing valuable knowledge.📚 Immersed in information technology, I delved into subjects like app development, database management, network administration, and web development. 💻📱🌐 I even glimpsed the business aspects.💼📈 The most invaluable skill I acquired was the ability to learn.🎓📚🧠 It will stay with me for a lifetime.',
   },
   {
     year: '2017',
     title: 'First Line of Code 👨🏾‍💻',
     duration: 'the beginning',
     details:
-      'In high school, I lucked out and transferred to a school with a cool computer science program. It was chaotic when I arrived, with everyone focused on their final project. 😅 I remember feeling completely lost as the teacher demoed some HTML. I wondered if I could handle it all. 🤔 But you know what? I stuck with it, and it turned out to be a great decision. 🙌🏾',
+      'In high school, I lucked out and transferred to a school with a cool computer science program. It was chaotic when I arrived, with everyone focused on their final project. 😅 I remember feeling completely lost as the teacher demoed some HTML. I wondered if I could handle it all. 🤔 But you know what? I stuck with it, and it turned out to be a great decision.🙌🏾',
   },
 ]

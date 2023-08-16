de68f78ab5dba184b6ce752162ee10c9c2462f91
Maz <mazspm@icloud.com>
2023-08-16 14:52:46 -0500

    updated 𝕏 logo 😬

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -28,19 +28,15 @@ export function SiteHeader() {
                 <span className="sr-only">GitHub</span>
               </div>
             </Link>
-            <Link
-              href={siteConfig.links.twitter}
-              target="_blank"
-              rel="noreferrer"
-            >
+            <Link href={siteConfig.links.x} target="_blank" rel="noreferrer">
               <div
                 className={buttonVariants({
                   size: 'sm',
                   variant: 'transparent',
                 })}
               >
-                <Icons.twitter className="h-5 w-5 fill-current" />
-                <span className="sr-only">Twitter</span>
+                <Icons.x className="h-5 w-5 fill-current" />
+                <span className="sr-only">X</span>
               </div>
             </Link>
             <Link

bcf3c17cf5ecff44150d463ca6cf42fba944d59a
Maz <mazspm@icloud.com>
2023-09-21 20:45:25 -0500

    done

--- components/canvas.tsx
+++ components/canvas.tsx
@@ -1,9 +1,11 @@
 'use client'
 
-import { useRef } from 'react'
+import { useEffect, useRef } from 'react'
 
 import { OrbitControls } from '@react-three/drei'
-import { Canvas, useFrame, useLoader } from '@react-three/fiber'
+import { Canvas, useLoader } from '@react-three/fiber'
+import { useMotionValue, useSpring } from 'framer-motion'
+import { motion } from 'framer-motion-3d'
 import { TextureLoader } from 'three/src/loaders/TextureLoader'
 
 export default function CanvasComponent() {
@@ -19,12 +21,25 @@ export default function CanvasComponent() {
 
 function Cube() {
   const mesh = useRef<THREE.Mesh>(null)
-  useFrame((state, delta) => {
-    if (mesh.current) {
-      mesh.current.rotation.x += delta * 0.25
-      mesh.current.rotation.y += delta * 0.25
-      mesh.current.rotation.z += delta * 0.25
-    }
+  const options = {
+    damping: 20,
+  }
+  const mouse = {
+    x: useSpring(useMotionValue(0), options),
+    y: useSpring(useMotionValue(0), options),
+  }
+
+  function manageMouseMove(event: MouseEvent) {
+    const { innerWidth, innerHeight } = window
+    const { clientX, clientY } = event
+    const x = -0.5 + clientX / innerWidth
+    const y = -0.5 + clientY / innerHeight
+    mouse.x.set(x)
+    mouse.y.set(y)
+  }
+  useEffect(() => {
+    window.addEventListener('mousemove', manageMouseMove)
+    return () => window.removeEventListener('mousemove', manageMouseMove)
   })
 
   const textureOne = useLoader(TextureLoader, '/me.png')
@@ -35,7 +50,7 @@ function Cube() {
   const textureSix = useLoader(TextureLoader, '/assets/color.png')
 
   return (
-    <mesh ref={mesh}>
+    <motion.mesh ref={mesh} rotation-x={mouse.y} rotation-y={mouse.x}>
       <boxGeometry args={[2.5, 2.5, 2.5]} />
       <meshStandardMaterial map={textureOne} attach="material-0" />
       <meshStandardMaterial map={textureTwo} attach="material-1" />
@@ -43,6 +58,6 @@ function Cube() {
       <meshStandardMaterial map={textureFour} attach="material-3" />
       <meshStandardMaterial map={textureFive} attach="material-4" />
       <meshStandardMaterial map={textureSix} attach="material-5" />
-    </mesh>
+    </motion.mesh>
   )
 }

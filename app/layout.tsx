5e27667222fb1f7e004072f219af899b6b08a962
Maz <mazspm@icloud.com>
2023-10-10 21:19:26 -0500

    fixing title

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ import { Analytics } from '@vercel/analytics/react'
 
 export const metadata: Metadata = {
   title: {
-    default: '🇨🇦 Maz | Dev | Ari',
+    default: '🇨🇦 mazwrld.dev',
     template: `%s - ${siteConfig.name}`,
   },
   description: siteConfig.description,

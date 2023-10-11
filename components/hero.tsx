0e74fd48ae9e12371ae89ba3ed743a0f7768da9c
Maz <mazspm@icloud.com>
2023-10-11 13:33:53 -0500

    missing arround with here section

--- components/hero.tsx
+++ components/hero.tsx
@@ -33,9 +33,9 @@ export default function Hero() {
         alt="Profile picture"
         className="relative mx-auto h-36 w-36 rounded-full"
       />
-      <div className="z-20">
-        <h2 className="pb-2 text-sm uppercase tracking-[15px] text-gray-400">
-          Software Developer
+      <div className="z-20 flex flex-col items-center justify-center">
+        <h2 className="lg:text-md text-sm uppercase tracking-[15px] text-gray-400 sm:text-xs sm:tracking-[10px] md:text-xs md:tracking-[10px]">
+          Baby Developer
         </h2>
         <h1 className="px-10 text-3xl font-semibold tracking-[1px] md:text-4xl lg:text-6xl">
           Mazen Mahari

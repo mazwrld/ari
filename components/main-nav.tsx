ee69ff27f9f51f7432cfb4c0f9e731cf3c275a42
Maz <mazspm@icloud.com>
2023-11-30 16:27:17 -0600

    working on glass dark bg

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -16,7 +16,7 @@ interface MainNavProps {
 export default function MainNav({ items }: MainNavProps) {
   return (
     <div className="flex gap-6 md:gap-10">
-      <Link href="/" className="hidden items-center space-x-2 md:flex">
+      <Link href="/" className="flex items-center space-x-2 md:flex">
         <Image src={Logo} height={34} width={34} alt="logo" />
         <span className="hidden font-bold sm:inline-block">
           {siteConfig.name}

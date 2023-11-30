e73126bf231c550de61123a58ac1d1747e4f1dee
Maz <mazspm@icloud.com>
2023-11-30 15:35:19 -0600

    removed white space

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -11,7 +11,7 @@ import ThemeToggle from '@/components/theme-toggle'
 export default function SiteHeader() {
   return (
     <header className="z-50 flex items-center justify-center">
-      <nav className="border-opacity/40 bg-opacity/80 dark:bg-opacity/75 dark:border-black/4 absolute top-0  w-full rounded-none border shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] dark:bg-gray-950 sm:top-6 sm:w-[36rem] sm:rounded-full">
+      <nav className="border-opacity/40 bg-opacity/80 dark:bg-opacity/75 dark:border-black/4 absolute top-0 w-full rounded-none border shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] dark:bg-gray-950 sm:top-6 sm:w-[36rem] sm:rounded-full">
         <div className="container flex h-12 items-center justify-between sm:justify-start">
           <MainNav items={siteConfig.mainNav} />
           <div className="flex flex-1 items-center justify-end space-x-4">

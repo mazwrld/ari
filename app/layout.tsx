0ca627ba21be6498416899293e77ac1a59c128d1
Maz <mazspm@icloud.com>
2023-11-30 18:34:03 -0600

    upgraded to next14 and added millionjs

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import ThemeProvider from '@/components/theme-provider'
 
 import '@/styles/globals.css'
 
-import { Metadata } from 'next'
+import type { Metadata, Viewport } from 'next'
 
 import { Analytics } from '@vercel/analytics/react'
 
@@ -17,15 +17,18 @@ export const metadata: Metadata = {
     template: `%s - ${siteConfig.name}`,
   },
   description: siteConfig.description,
-  themeColor: [
-    { media: '(prefers-color-scheme: light)', color: 'white' },
-    { media: '(prefers-color-scheme: dark)', color: 'black' },
-  ],
   icons: {
     icon: '/favicon.ico',
     shortcut: '/favicon-16x16.png',
     apple: '/apple-touch-icon.png',
   },
+}
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: 'white' },
+    { media: '(prefers-color-scheme: dark)', color: 'black' },
+  ],
 }
 
 interface RootLayoutProps {

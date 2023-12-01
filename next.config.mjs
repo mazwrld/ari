af54679477138e72d7f247da552e5fc809d70bdc
Maz <mazspm@icloud.com>
2023-11-30 18:34:03 -0600

    upgraded to next14 and added millionjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import million from 'million/compiler'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -10,4 +12,4 @@ const nextConfig = {
   swcMinify: true,
 }
 
-export default nextConfig
+export default million.next(nextConfig, { auto: { rsc: true } })

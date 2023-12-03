a2c60211005ba1bd69bdd7b425a2333e2e677b5d
Maz <mazspm@icloud.com>
2023-12-02 19:20:02 -0600

    cleaning up

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,8 +42,8 @@ export default function RootLayout({ children }: RootLayoutProps) {
             'scrollbar-none min-h-screen scroll-smooth bg-background antialiased'
           )}
         >
-          <div className="absolute right-[11rem] top-[-6rem] -z-10 h-[31.25rem] w-[31.25rem] rounded-full bg-[#fbe2e3] blur-[10rem] dark:bg-[#946263] sm:w-[68.75rem]"></div>
-          <div className="absolute left-[-35rem] top-[-1rem] -z-10 h-[31.25rem] w-[50rem] rounded-full bg-[#dbd7fb] blur-[10rem] dark:bg-[#676394] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+          <div className="absolute right-[-5rem] top-[-6rem] -z-10 h-[31.25rem] w-[31.25rem] rounded-full bg-[#fbe2e3] blur-[50rem] dark:bg-[#946263] sm:w-[68.75rem]"></div>
+          <div className="absolute left-[-35rem] top-[-1rem] -z-10 h-[31.25rem] w-[50rem] rounded-full bg-[#dbd7fb] blur-[40rem] dark:bg-[#676394] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             <div className="relative flex min-h-screen flex-col">
               <SiteHeader />

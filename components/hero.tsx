6062b0def930ecefcd76d049eb6efc838f88f1eb
Maz <mazspm@icloud.com>
2024-06-14 15:35:46 -0500

    chore: Refactor layout and hero components
    Refactor the layout component to remove unnecessary div elements and improve page structure. Also, update the hero component to include the rays component and adjust the layout of the profile picture and text.

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,25 +1,30 @@
 import Image from 'next/image'
 import ProfileImg from '@/public/me.png'
 
+import Rays from '@/components/rays'
+
 export default function Hero() {
   return (
-    <div className="flex h-screen flex-col items-center justify-center space-y-8 overflow-hidden py-6 text-center md:py-0">
-      <Image
-        src={ProfileImg}
-        priority
-        height={144}
-        width={144}
-        alt="Profile picture"
-        className="relative mx-auto h-36 w-36 rounded-full"
-      />
-      <div className="z-20">
-        <h2 className="pb-2 text-xs uppercase tracking-[15px] text-muted-foreground sm:text-xs">
-          Baby Developer
-        </h2>
-        <h1 className="px-10 text-3xl font-semibold tracking-[1px] sm:text-[2.4rem] lg:text-6xl">
-          Mazen Mahari
-        </h1>
+    <>
+      <Rays />
+      <div className="flex h-screen flex-col items-center justify-center space-y-8 overflow-hidden py-6 text-center md:py-0">
+        <Image
+          src={ProfileImg}
+          priority
+          height={144}
+          width={144}
+          alt="Profile picture"
+          className="relative mx-auto h-36 w-36 rounded-full"
+        />
+        <div className="z-20">
+          <h2 className="pb-2 text-xs uppercase tracking-[15px] text-muted-foreground sm:text-xs">
+            Baby Developer
+          </h2>
+          <h1 className="px-10 text-3xl font-semibold tracking-[1px] sm:text-[2.4rem] lg:text-6xl">
+            Mazen Mahari
+          </h1>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
--- components/skills.tsx
+++ components/skills.tsx
@@ -2,7 +2,7 @@ import { skillsData } from '@/config/skills'
 
 export default function Skills() {
   return (
-    <div className="relative flex h-[17rem] w-full items-center justify-center bg-dot-black/[0.2] lg:h-[20rem] dark:bg-dot-white/[0.2]">
+    <div className="relative flex h-96 w-full items-center justify-center bg-dot-black/[0.2] dark:bg-dot-white/[0.2]">
       <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_top,transparent_20%,black)] dark:bg-black"></div>
       <div className="mx-auto flex max-w-7xl flex-col items-center justify-center gap-2">
         <h2 className="text-5xl font-bold tracking-normal md:text-6xl lg:text-5xl lg:tracking-tight">

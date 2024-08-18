5dc74dbd747888e780575819401cdff6d2f7a1b6
Maz <34517221+mazwrld@users.noreply.github.com>
2024-08-17 19:43:26 -0500

    adds some texture to the nav (#23)

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -9,7 +9,37 @@ import ThemeToggle from '@/components/theme-toggle'
 export default function SiteHeader() {
   return (
     <header className="sticky top-0 z-50 flex items-center justify-center">
-      <nav className="shadow-black-3 supports-[backdrop-filter] absolute top-0 w-full rounded-none border-b bg-background/40 shadow-lg backdrop-blur-xl transition-all duration-200 sm:top-4 sm:w-[36rem] sm:rounded-full">
+      <svg xmlns="http://www.w3.org/2000/svg" width="0" height="0">
+        <defs>
+          <filter id="texture">
+            <feTurbulence
+              type="fractalNoise"
+              baseFrequency="0.3"
+              numOctaves="1"
+              seed="0"
+              stitchTiles="stitch"
+              result="feTurbulence-391ac80d"
+            ></feTurbulence>
+            <feGaussianBlur
+              in="SourceGraphic"
+              stdDeviation="2"
+              edgeMode="duplicate"
+              result="feGaussianBlur-85e146a6"
+            ></feGaussianBlur>
+            <feDisplacementMap
+              in="feGaussianBlur-85e146a6"
+              in2="feTurbulence-391ac80d"
+              scale="37"
+              xChannelSelector="A"
+              yChannelSelector="A"
+            ></feDisplacementMap>
+          </filter>
+        </defs>
+      </svg>
+      <nav
+        style={{ backdropFilter: 'url("#texture")' }}
+        className="shadow-black-3 supports-[backdrop-filter] absolute top-0 w-full rounded-none border-b bg-background/40 shadow-lg backdrop-blur-xl transition-all duration-200 sm:top-4 sm:w-[36rem] sm:rounded-full"
+      >
         <div className="container flex h-14 items-center justify-between sm:justify-start">
           <MainNav items={siteConfig.mainNav} />
           <div className="flex flex-1 items-center justify-end space-x-4">

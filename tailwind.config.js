04a1284906c08d1dff27e52d367214d7a1d14928
Maz <mazspm@icloud.com>
2025-02-25 21:21:54 -0600

    tailwind upgrade works

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
 const svgToDataUri = require('mini-svg-data-uri')
 const { fontFamily } = require('tailwindcss/defaultTheme')
-const {
-  default: flattenColorPalette,
-} = require('tailwindcss/lib/util/flattenColorPalette')
+const flattenColorPalette = require('tailwindcss/lib/util/flattenColorPalette')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -96,8 +94,8 @@ module.exports = {
               `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
             )}")`,
           }),
-        }
-        // { values: flattenColorPalette(theme('backgroundColor')), type: 'color' }
+        },
+        { values: flattenColorPalette(theme('backgroundColor')), type: 'color' }
       )
     },
   ],

241bc0d93bb1d1cff75c7d319a2535616b29ff4b
Maz <mazspm@icloud.com>
2025-02-25 21:16:25 -0600

    updates tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import Particles from '@/components/particles'
 
 export default function Home() {
   return (
-    <div className="flex h-screen w-screen flex-col items-center justify-center overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
-      <div className="animate-glow animate-fade-left hidden h-px w-screen bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0 md:block" />
+    <div className="flex h-screen w-screen flex-col items-center justify-center overflow-hidden bg-linear-to-tl from-black via-zinc-600/20 to-black">
+      <div className="animate-glow animate-fade-left hidden h-px w-screen bg-linear-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0 md:block" />
       <Particles
         className="animate-fade-in absolute inset-0 -z-10"
         quantity={100}
@@ -14,7 +14,7 @@ export default function Home() {
         Mazen Mahari
       </h1>
 
-      <div className="animate-glow animate-fade-right hidden h-px w-screen bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0 md:block" />
+      <div className="animate-glow animate-fade-right hidden h-px w-screen bg-linear-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0 md:block" />
       <div className="animate-fade-in my-16 text-center">
         <h2 className="text-sm text-zinc-500">
           I'm working at{' '}
